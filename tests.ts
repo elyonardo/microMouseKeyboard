f35ee83f9d8d84422185b1e1dd390c7fcf879413
Elia <35810628+elyonardo@users.noreply.github.com>
2019-11-13 17:19:32 +0100

    Update tests.ts

--- tests.ts
+++ tests.ts
@@ -23,3 +23,21 @@ input.onButtonPressed(Button.B, () => {
     // ctrl-alt-del
     bluetooth.keyboardSendOneKeyCode([ Modifier.MODIFIER_LEFT_CTRL, Modifier.MODIFIER_LEFT_ALT ], KeyCode.KEY_DELETE)
 })
+
+input.onPinPressed(TouchPin.P0, () => {
+    bluetooth.setMouseButton(MouseButton.MOUSE_BUTTON_LEFT, ButtonState.BUTTON_DOWN)
+})
+input.onPinReleased(TouchPin.P0, () => {
+    bluetooth.setMouseButton(MouseButton.MOUSE_BUTTON_LEFT, ButtonState.BUTTON_UP)
+})
+
+input.onPinPressed(TouchPin.P1, () => {
+    bluetooth.setMouseButton(MouseButton.MOUSE_BUTTON_RIGHT, ButtonState.BUTTON_DOWN)
+})
+input.onPinReleased(TouchPin.P1, () => {
+    bluetooth.setMouseButton(MouseButton.MOUSE_BUTTON_RIGHT, ButtonState.BUTTON_UP)
+})
+
+basic.forever(() => {
+    bluetooth.setMouseSpeed(input.acceleration(Dimension.X) / 8, input.acceleration(Dimension.Y) / 8, 0)
+})
